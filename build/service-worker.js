611294f651f5ab24e6f59ff6f02066f68db0eea7
Sam <samuel_ramirez_07@yahoo.com>
2020-09-06 16:54:42 -0700

    SeeFood Test 2
    File

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seefood-app/precache-manifest.7e5a6cdde0c43548cd843f08709aaf78.js"
+  "/sam-ramirez/precache-manifest.7e5a6cdde0c43548cd843f08709aaf78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/seefood-app/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sam-ramirez/index.html"), {
+
+  blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/],
 });
